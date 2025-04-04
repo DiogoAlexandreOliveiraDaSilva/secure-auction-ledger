1fd36c68338bd09d77a3e9caca2e3b63d6e88150
Diogo Silva <afkflex@gmail.com>
2025-04-04 21:00:44 +0100

    RPC base

--- public_ledger/src/kademlia/mod.rs
+++ public_ledger/src/kademlia/mod.rs
@@ -1,8 +1,45 @@
 pub(crate) mod routing_table;
+use tonic::{transport::Server,Request,Response,Status};
 
-pub fn init(port: u32) {
-    let ip = "127.0.0.1"; // TODO:Change to function that get's local ip
-    let curr_table = routing_table::RoutingTable::new(ip.to_string(), port as u16);
-    println!("Node created with id: {:x?}, ip: {}, port: {}", curr_table.get_curr_node().get_id(), ip, port);
+pub mod communication{
+    tonic::include_proto!("communication");
 }
 
+use communication::kademlia_server::{Kademlia, KademliaServer};
+use communication::{PingRequest, PingResponse, FindNodeRequest, FindNodeResponse, StoreRequest, StoreResponse,FindValueRequest ,FindValueResponse };
+
+#[derive(Default)]
+pub struct MyKademliaService;
+
+#[tonic::async_trait]
+impl Kademlia for MyKademliaService {
+    async fn ping(&self, request: Request<PingRequest>) -> Result<Response<PingResponse>, Status> {
+        let reply = PingResponse {
+            message: format!("Pong"),
+        };
+        Ok(Response::new(reply))
+    }
+
+    async fn find_node(&self, request: Request<FindNodeRequest>) -> Result<Response<FindNodeResponse>, Status> {
+        let reply = FindNodeResponse {
+            nodes: vec![],
+        };
+        Ok(Response::new(reply))
+    }
+
+    async fn store(&self, request: Request<StoreRequest>) -> Result<Response<StoreResponse>, Status> {
+        let reply = StoreResponse {
+            message: "Stored successfully".to_string(),
+        };
+        Ok(Response::new(reply))
+    }
+
+    async fn find_value(&self, request: Request<FindValueRequest>) -> Result<Response<FindValueResponse>, Status> {
+        let reply = FindValueResponse {
+            value: "value".as_bytes().to_vec(),
+            nodes: vec![],
+        };
+        Ok(Response::new(reply))
+    }
+}
+   
--- public_ledger/src/main.rs
+++ public_ledger/src/main.rs
@@ -1,4 +1,5 @@
-use kademlia::init;
+use tonic::transport::Server;
+use kademlia::{communication::kademlia_server, MyKademliaService};
 
 // Tests
 #[cfg(test)]
@@ -7,7 +8,16 @@ mod tests;
 mod blockchain;
 mod kademlia;
 
-
-fn main() {
-    init(8080);
+#[tokio::main]
+async fn main() -> Result<(),Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let kademlia_service = MyKademliaService::default();
+    let kademlia_server = kademlia_server::KademliaServer::new(kademlia_service);
+    println!("Server listening on {}", addr);
+    Server::builder()
+        .add_service(kademlia_server)
+        .serve(addr)
+        .await
+        .unwrap();
+    Ok(())
 }
