a8715164152655163ff6a6aca837cca9a44abce7
Diogo Silva <afkflex@gmail.com>
2025-04-10 18:53:39 +0100

    Done

--- public_ledger/src/main.rs
+++ public_ledger/src/main.rs
@@ -1,5 +1,7 @@
+use kademlia::start_kademlia_server;
 use tonic::transport::Server;
-use kademlia::{communication::kademlia_server, MyKademliaService};
+use kademlia::{communication::kademlia_server, MyKademliaService, communication::kademlia_client::KademliaClient};
+use kademlia::communication::{FindValueRequest, PingRequest}; // Needed for proper request building
 
 // Tests
 #[cfg(test)]
@@ -9,15 +11,37 @@ mod blockchain;
 mod kademlia;
 
 #[tokio::main]
-async fn main() -> Result<(),Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
-    let kademlia_service = MyKademliaService::default();
-    let kademlia_server = kademlia_server::KademliaServer::new(kademlia_service);
-    println!("Server listening on {}", addr);
-    Server::builder()
-        .add_service(kademlia_server)
-        .serve(addr)
-        .await
-        .unwrap();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // User Inputs 1 or 0s
+    println!("Enter 1 (server) or 0 (client):");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    let input = input.trim();
+
+    // Switch case
+    match input {
+        "1" => {
+            let addr = "::1".to_string();
+            let port = 50051;
+            kademlia::start_kademlia_server(addr, port).await?;
+        }
+        "0" => {
+            let channel = tonic::transport::Channel::from_static("http://[::1]:50051")
+                .connect()
+                .await?;
+
+            let mut client = KademliaClient::new(channel);
+
+            // Create the request object
+            let request = tonic::Request::new(PingRequest {});
+
+            let response = client.ping(request).await?;
+            println!("Response: {:?}", response.into_inner());
+        }
+        _ => {
+            println!("Invalid input. Please enter 1 or 0.");
+        }
+    }
+
     Ok(())
 }
